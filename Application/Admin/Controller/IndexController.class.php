8f5e96dca97eee9b29dedf677804280a9b9eca5c
qq396986807 <396986807@qq.com>
2017-05-18 11:28:05 +0800

    cxf

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -2,17 +2,8 @@
 namespace Admin\Controller;
 use Think\Controller;
 class IndexController extends Controller {
-<<<<<<< HEAD
-
-    public function aaa(){
-
-=======
-	/**
-	 * 显示首页
-	 */
-    public function index(){
-        $this->show();
->>>>>>> origin/master
+    public function fun2(){
+        
     }
     
 }
