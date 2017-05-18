b6d6d183888928e75dfcd39a5650f98d8bdd27ee
527291001 <527291001@qq.com>
2017-05-18 11:57:15 +0800

    诗凯
    控制器

--- Application/Home/Controller/ChatController.class.php
+++ Application/Home/Controller/ChatController.class.php
@@ -1,14 +1,11 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
-class IndexController extends Controller {
+class ChatController extends Controller {
 	/**
 	 * 注释
 	 * @param 参数1
 	 * @param 参数2
 	 * @return 返回类型
 	 */
-    public function index(){
-        $this->show();
-    }
 }
