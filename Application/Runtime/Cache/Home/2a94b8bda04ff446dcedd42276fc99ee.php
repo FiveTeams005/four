81ea424ac036f39c27a49c7b422fb43ccb5f872b
qq396986807 <396986807@qq.com>
2017-05-19 08:48:33 +0800

    cxf

---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,71 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>首页</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="/four/Public/static/resetcss/normalize.css">
+    <link rel="stylesheet" href="/four/Public/static/bootstrap/css/bootstrap.min.css">
+
+    <script src="/four/Public/static/jquery/jquery.min.js"></script>
+    <script src="/four/Public/static/bootstrap/js/bootstrap.min.js"></script>
+    <script src="/four/Public/static/vue/vue.js"></script>
+</head>
+<body>
+    <!--头部-->
+    <header>
+
+    </header>
+    <!--body部-->
+    <div class="container" id="content">
+
+    </div>
+    <!--尾部，（底部固定导航）-->
+    <footer >
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2 col-xs-2">
+                    <div class="row">
+                        <img src="" alt="" />
+                    </div>
+                    <div class="row">
+                        首页
+                    </div>
+                </div>
+                <div class="col-md-2 col-xs-2">
+                    <div class="row">
+                        <img src="" alt="">
+                    </div>
+                    <div class="row">
+                        客服
+                    </div>
+                </div>
+                <div class="col-md-4 col-xs-4">
+                    <div class="row">
+                        <img src="" alt="">
+                    </div>
+                    <div class="row">
+                        发布
+                    </div>
+                </div>
+                <div class="col-md-2 col-xs-2">
+                    <div class="row">
+                        <img src="" alt="">
+                    </div>
+                    <div class="row">
+                        信息
+                    </div>
+                </div>
+                <div class="col-md-2 col-xs-2">
+                    <div class="row">
+                        <img src="" alt="">
+                    </div>
+                    <div class="row">
+                        我的
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
