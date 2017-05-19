fc1410a47fa099e9673afad755b20ed943dd17bc
sun-zw <619650434@qq.com>
2017-05-19 10:53:03 +0800

    首页 修改

--- Public/Home/js/index/index.js
+++ Public/Home/js/index/index.js
@@ -3,6 +3,8 @@
  */
 
 $(function(){
+    //获取定位；
+
 
     // 轮播滑动函数
     slide();
@@ -18,5 +20,24 @@ $(function(){
     }
     //自动轮播；每隔3秒自动轮播
     $('#myCarousel').carousel({interval:3000});
+    //导航滚动顶部置顶；
+    $(window).scroll(function () {
+        var menu_top = $('#menu-wrap').offset().top;
+        if ($(window).scrollTop() >= menu_top) {
+            $('.menu').addClass('menuFixed');
+        }
+        else {
+            $('.menu').removeClass('menuFixed');
+        }
+    });
+    //（新鲜的、附近的）导航点击事件；
+    $('#menu-wrap').on('click','nav ul li',function () {
+        $(this).addClass('active').siblings().removeClass('active');
+        if($(this).index() == 0){   //新鲜的
+            alert(1);
+        }else if($(this).index() == 1){     //附近的；
+            alert(2);
+        }
+    });
 
 })
