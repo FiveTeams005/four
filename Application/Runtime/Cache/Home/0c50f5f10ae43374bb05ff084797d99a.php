5da6003e8efafb941e07b816ca76b03ae2f6ecc4
LuisChuan <727751846@qq.com>
2017-05-19 10:25:16 +0800

    liuzhi

--- Application/Runtime/Cache/Home/0c50f5f10ae43374bb05ff084797d99a.php
+++ Application/Runtime/Cache/Home/0c50f5f10ae43374bb05ff084797d99a.php
@@ -1,5 +1,4 @@
 <?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
-<<<<<<< HEAD
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,6 +6,7 @@
     <title>Title</title>
     <link rel="stylesheet" href="/four/Public/static/resetcss/normalize.css">
     <link rel="stylesheet" href="/four/Public/static/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet"  href="/four/Public/Home/css/static/footer/footerStyle.css">
     <script src="/four/Public/static/jquery/jquery.min.js"></script>
     <script src="/four/Public/static/bootstrap/js/bootstrap.min.js"></script>
     <script src="/four/Public/static/vue/vue.js"></script>
@@ -20,6 +20,11 @@
         }
         .logo2{
             margin: 7px;
+            height: 16px;
+            line-height: 30px;
+        }
+        .logo3{
+            margin-top: 15px;
             height: 16px;
             line-height: 30px;
         }
@@ -80,10 +85,19 @@
             margin-top: 10px;
             height: 45px;
         }
+        .img-circle{
+            width: 45px;
+            height: 45px;
+        }
+        .detail{
+            font-size: 10px;
+            color: #9d9d9d;
+        }
     </style>
 </head>
 <body class="bg">
-<header>
+<!--未登录时显示头部-->
+<header style="display: none">
 <div class="container-fluid moheader">
     <div class="row text-center">
         <div class="col-xs-6">
@@ -102,14 +116,29 @@
     </div>
 </div>
 </header>
+<!--登录状态显示头部-->
+<header style="display: block">
+    <div class="container">
+        <div class="row text-left main">
+            <div class="col-xs-8">
+                <h5>我的昵称</h5>
+                <p class="detail">虽然没挣到钱，但在闲鱼开心就好</p>
+            </div>
+            <div class="col-xs-4  text3">
+                <div class="col-xs-6 text-right"><img src="/four/Public/home/img/xianyu/entry_icon_sold.png" class="img-circle"></div>
+                <div class="col-xs-2 col-xs-offset-1 text-right"><img src="/four/Public/home/img/xianyu/entry_arrow.png" class="logo3"></div>
+            </div>
+        </div>
+    </div>
+</header>
 <div class="container-fluid content">
     <div class="row text-left main">
         <div class="col-xs-2"><img src="/four/Public/home/img/xianyu/entry_icon_on_shelf.png" class="logo1"></div>
-       <div class="col-xs-10 text">
-        <div class="col-xs-8 text1">我发布的</div>
-        <div class="col-xs-1 text-right text2">0</div>
-        <div class="col-xs-2 text-right"><img src="/four/Public/home/img/xianyu/entry_arrow.png" class="logo2"></div>
-       </div>
+        <div class="col-xs-10 text">
+            <div class="col-xs-8 text1">我发布的</div>
+            <div class="col-xs-1 text-right text2">0</div>
+            <div class="col-xs-2 text-right"><img src="/four/Public/home/img/xianyu/entry_arrow.png" class="logo2"></div>
+        </div>
     </div>
     <div class="row text-left main">
         <div class="col-xs-2"><img src="/four/Public/home/img/xianyu/entry_icon_sold.png" class="logo1"></div>
@@ -162,71 +191,54 @@
             <div class="col-xs-2  text-right"><img src="/four/Public/home/img/xianyu/entry_arrow.png" class="logo2"></div>
         </div>
     </div>
-
 </div>
-</body>
-=======
-<html class="um landscape min-width-240px min-width-320px min-width-480px min-width-768px min-width-1024px">
-    <head>
-        <title></title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="target-densitydpi=device-dpi, width=device-width, initial-scale=1, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
-        <link rel="stylesheet" href="/four/Public/Home/css/static/appcan/fonts/font-awesome.min.css">
-        <link rel="stylesheet" href="/four/Public/Home/css/static/appcan/ui-box.css">
-        <link rel="stylesheet" href="/four/Public/Home/css/static/appcan/ui-base.css">
-        <link rel="stylesheet" href="/four/Public/Home/css/static/appcan/ui-color.css">
-        <link rel="stylesheet" href="/four/Public/Home/css/static/appcan/appcan.icon.css">
-        <link rel="stylesheet" href="/four/Public/Home/css/static/appcan/appcan.control.css">
-        <link rel="stylesheet" href="/four/Public/Home/css/center/center.css">
-    </head>
-    <body class="" ontouchstart>
-        <div class="bc-bg" tabindex="0" data-control="PAGE" id="Page">
-            <div class="uh bc-head  ubb bc-border" data-control="HEADER" id="Header">
-                <div class="ub">
-                    <div class="nav-btn" id="nav-left">
-                        <div class="fa fa-1g ub-img1"></div>
-                    </div>
-                    <h1 class="ut ub-f1 ulev-3 ut-s tx-c" tabindex="0">登录</h1>
-                    <div class="nav-btn" id="nav-right">
-                        <div class="fa  fa-1g  ub-img1"></div>
-                    </div>
+
+<footer >
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-sm-2 col-xs-2">
+                <div class="row">
+                    <img src="/four/Public/Home/img/xianyu/comui_tab_home.png" alt="" />
+                </div>
+                <div class="row">
+                    首页
                 </div>
             </div>
-            <div class="" data-control="CONTENT" id="ScrollContent">
-  <div class="scrollbox">
-    <div class="box_bounce ub ub-ver ub-pc">
-      <div class="ub-f1">
-      </div>
-      <div class="bounce_status">下拉更新......</div>
-      <div class="bounce_status">松手更新......</div>
-      <div class="bounce_status">更新中......</div>
-    </div>
-    <div class="ub ub-ver">
-      <div class=" umar-at1">
-        <div id="listview1" class="ubt bc-border c-wh">
+            <div class="col-sm-2 col-xs-2">
+                <div class="row">
+                    <img src="/four/Public/Home/img/xianyu/comui_tab_custom_head.png" alt="">
+                </div>
+                <div class="row">
+                    客服
+                </div>
+            </div>
+            <div class="col-sm-4 col-xs-4 publish">
+                <div class="row">
+                    <img src="/four/Public/Home/img/xianyu/comui_tab_post.png" alt="">
+                </div>
+                <div class="row publish-txt">
+                    发布
+                </div>
+            </div>
+            <div class="col-sm-2 col-xs-2 ">
+                <div class="row">
+                    <img src="/four/Public/Home/img/xianyu/comui_tab_message.png" alt="">
+                </div>
+                <div class="row">
+                    信息
+                </div>
+            </div>
+            <div class="col-sm-2 col-xs-2">
+                <div class="row">
+                    <img src="/four/Public/Home/img/xianyu/comui_tab_person.png" alt="">
+                </div>
+                <div class="row">
+                    我的
+                </div>
+            </div>
         </div>
-        <div id="listview2" class="ubt bc-border c-wh umar-at1">
-        </div>
-      </div>
-      <div id="listview3" class="ubt bc-border c-wh umar-at1">
-      </div>
-      <div id="listview4" class="ubt bc-border c-wh umar-at1">
-      </div>
     </div>
-  </div>
-</div>
+</footer>
 
-        </div>
-        <script src="/four/Public/Home/js/static/appcan/appcan.js"></script>
-        <script src="/four/Public/Home/js/static/appcan/appcan.control.js"></script>
-        <script src="/four/Public/Home/js/static/appcan/appcan.scrollbox.js"></script>
-        <script src="/four/Public/Home/js/static/appcan/template.import.js"></script>
-        <script src="/four/Public/Home/js/static/appcan/appcan.listview.js"></script>
-        <script src="/four/Public/Home/js/static/appcan/appcan.optionList.js"></script>
-        <script src="/four/Public/Home/js/static/appcan/appcan.slider.js"></script>
-        <script src="/four/Public/Home/js/static/appcan/appcan.tab.js"></script>
-        <script src="/four/Public/Home/js/static/appcan/appcan.treeview.js"></script>
-        <script src="/four/Public/Home/js/center/center.js"></script>
-    </body>
->>>>>>> origin/master
+</body>
 </html>
