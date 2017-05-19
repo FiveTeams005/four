0937e9c3feeb9f47882d2f9e61538406c05bc85f
LuisChuan <727751846@qq.com>
2017-05-19 10:52:16 +0800

    liuzhi

---
+++ Application/Runtime/Cache/Home/5980c0b5e63d3c7a8abff724e4486b02.php
@@ -0,0 +1,25 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title>Title</title>
+    <link rel="stylesheet" href="/four/Public/static/resetcss/normalize.css">
+    <link rel="stylesheet" href="/four/Public/static/bootstrap/css/bootstrap.min.css">
+    <script src="/four/Public/static/jquery/jquery.min.js"></script>
+    <script src="/four/Public/static/bootstrap/js/bootstrap.min.js"></script>
+    <script src="/four/Public/static/vue/vue.js"></script>
+    <style>
+        .bg{
+            background: url(/four/Public/home/img/xianyu/match_no_more.png);
+        }
+    </style>
+</head>
+<body>
+<div class="container bg">
+    <div class="row">
+        <div class="col-xs"></div>
+    </div>
+</div>
+</body>
+</html>
