12225829c72f2eb3c18faeb974d45a09163f6715
sun-zw <619650434@qq.com>
2017-05-19 09:04:52 +0800

    footer样式路径修改

--- Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <link rel="stylesheet" href="/Public/static/resetcss/normalize.css">
     <link rel="stylesheet" href="/Public/static/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/Public/Home/static/css/footerStyle.css">
-    <link rel="stylesheet" href="/Public/Home/css/indexStyle.css">
+    <link rel="stylesheet" href="/Public/Home/css/static/footer/footerStyle.css">
+    <link rel="stylesheet" href="/Public/Home/css/index/indexStyle.css">
 
     <script src="/Public/static/jquery/jquery.min.js"></script>
     <script src="/Public/static/bootstrap/js/bootstrap.min.js"></script>
