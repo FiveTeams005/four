e25e5932e0b044db588b46301e7f4a29b2ed7ae7
LuisChuan <727751846@qq.com>
2017-05-20 16:54:10 +0800

    个人中心多个页面上传

---
+++ Application/Home/Controller/CenterController.class.php
@@ -0,0 +1,35 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class CenterController extends Controller {
+	/**
+	 * 注释
+	 * @param 参数1
+	 * @param 参数2
+	 * @return 返回类型
+	 */
+	/**
+	 * 个人中心页面加载
+	 */
+	public function Center(){
+		$this->display();
+	}
+	public function PersonalInfo(){
+        $this->display();
+    }
+    public function MyPublish(){
+	    $this->display();
+    }
+    public function UnderSale(){
+        $this->display();
+    }
+    public function MyBuy(){
+        $this->display();
+    }
+    public function MyZan(){
+        $this->display();
+    }
+    public function MyAuction(){
+        $this->display();
+    }
+}
