080963b180cec5fadcd6b815fd327073fbe0a350
527291001 <527291001@qq.com>
2017-05-22 15:06:03 +0800

    add indexController&&del ...

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -2,6 +2,16 @@
 namespace Admin\Controller;
 use Think\Controller;
 class IndexController extends Controller {
-  
-    
+	/**
+	 * 登录用户对应的权限菜单
+	 * 
+	 */
+	public function index(){
+		$db = M('menu');
+		$res1 = $db->where( 'b_pid = 0')->select();
+		$res2 = $db->where( 'b_pid != 0')->select();
+		$this->assign('menu1',$res1);
+		$this->assign('menu2',$res2);
+		$this->display();
+	}
 }
