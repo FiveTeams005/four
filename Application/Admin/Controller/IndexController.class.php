bfac18f14111fba1f47806df8c41d2e037108c7c
sun-zw <619650434@qq.com>
2017-05-22 13:02:51 +0800

    Update IndexController.class.php

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -4,8 +4,7 @@ use Think\Controller;
 class IndexController extends Controller {
 	/**
 	 * 登录用户对应的权限菜单
-	 * 
-	 */
+	 **/
 	public function index(){
 		$db = M('menu');
 		$res1 = $db->where( 'b_pid = 0')->select();
