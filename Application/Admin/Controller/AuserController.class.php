5413d71ed357acc03d06d4d9705f3b1e9daef019
sun-zw <619650434@qq.com>
2017-05-24 00:05:07 +0800

    add chat.html & Huser.html & Auser.html
    添加了聊天页面 后台 用户管理页面

---
+++ Application/Admin/Controller/AuserController.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class AuserController extends Controller {
+	/**
+	 * 前台用户列表
+	 */
+	public function auser(){
+		$this->display();
+	}
+}
+?>
