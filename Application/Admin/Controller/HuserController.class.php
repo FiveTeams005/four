b6fb20b8b2748462d219bd1e2b0858c129f9aa4c
527291001 <527291001@qq.com>
2017-05-25 11:39:19 +0800

    sk
    update admin

--- Application/Admin/Controller/HuserController.class.php
+++ Application/Admin/Controller/HuserController.class.php
@@ -1,12 +1,43 @@
 <?php
 namespace Admin\Controller;
-use Think\Controller;
-class HuserController extends Controller {
+use Common\Controller\BaseController;
+class HuserController extends BaseController {
 	/**
 	 * 前台用户列表
 	 */
 	public function huser(){
+		$where = "";
+		if(isset($_GET['userStatus']) && $_GET['userStatus'] != ""){
+			$where['h_status'] = array('eq',$_GET['userStatus']);
+		}
+		if(isset($_GET['selectInp'])){
+			$where['_string'] = "(h_account like '%{$_GET['selectInp']}%') OR (h_nick like '%{$_GET['selectInp']}%')";
+		}
+		$huser = M('huser'); // 实例化auser对象
+		$list = $huser->page((isset($_GET['p'])?$_GET['p']:1).',5')->where($where)->select();
+		$this->assign('data',$list);// 赋值数据集
+		$count = $huser->where($where)->count();// 查询满足要求的总记录数
+		$Page = new \Think\Page($count,5);// 实例化分页类 传入总记录数和每页显示的记录数
+		$show = $Page->show();// 分页显示输出
+		$this->assign('page',$show);// 赋值分页输出
 		$this->display();
+	}
+
+	/**
+	 * 锁定/解锁用户
+	 * @return string
+	 */
+	public function huserLock(){
+		$h_id = $_POST['h_id'];
+		$huser = M('huser');
+		$data['h_status'] = $_POST['h_stutas'];
+		$lockRes = $huser->data($data)->where("h_id in({$h_id})")->save();
+		if($lockRes){
+			echo "操作成功！";
+		}
+		else{
+			echo "操作失败！";
+		}
 	}
 }
 ?>
---
+++ Public/Admin/myjs/huser.js
@@ -0,0 +1,87 @@
+$(function(){
+	// 全选复选框点击事件
+	$("#chooseAll").click(function(){
+		if($(this).prop("checked")){
+			$("tbody input[type='checkbox']").prop("checked",true);
+		}
+		else{
+			$("tbody input[type='checkbox']").prop("checked",false);
+		}
+	})
+	// 单行复选框点击事件
+	$("tbody").on('click',"input[type='checkbox']",function(){
+		for(var i = 0;i < $("tbody input[type='checkbox']").length;i++){
+			if($("tbody input[type='checkbox']:eq("+i+")").prop('checked') == false){
+				$("#chooseAll").prop("checked",false);
+				break;
+			}
+			else if(i == $("tbody input[type='checkbox']").length-1){
+				$("#chooseAll").prop("checked",true);
+			}
+		}
+	})
+	// 单行锁定/解锁事件
+	$("tbody").on('click','.lock',function(){
+		var status;
+		if($(this).attr("status") == 1){
+			status = 0;
+		}
+		else{
+			status = 1;
+		}
+		$.ajax({
+			url:urlLock,
+			type:'POST',
+			data:{h_id:$(this).attr("hId"),h_stutas:status},
+			success:function(data){
+				layer.alert(data,{
+    				time:0,
+    				btn: ['确定'],
+    				yes:function(index){
+    					layer.close(index);
+    					if(data == '操作成功！'){
+    						location.href = url;
+    					}
+    				}
+	    		})
+			}
+		})
+	})
+	// 解锁按钮点击事件
+	$("#unLock").click(function(){
+		huserLock(1);
+	})
+	// 锁定按钮点击事件
+	$("#Lock").click(function(){
+		huserLock(0);
+	})
+	// 锁定/解锁 函数
+	function huserLock(h_stutas){
+		var ary = [];
+		for(var i = 0;i < $("tbody input[type='checkbox']").length;i++){
+			if($("tbody input[type='checkbox']:eq("+i+")").prop("checked")){
+				ary.push($("tbody input[type='checkbox']:eq("+i+")").attr("rid"));
+			}
+		}
+		if(ary.length != 0){
+			var str = ary.join(",");
+			$.ajax({
+				url:urlLock,
+				type:'POST',
+				data:{h_id:str,h_stutas:h_stutas},
+				success:function(data){
+					layer.alert(data,{
+	    				time:0,
+	    				btn: ['确定'],
+	    				yes:function(index){
+	    					layer.close(index);
+	    					if(data == '操作成功！'){
+	    						location.href = url;
+	    					}
+	    				}
+		    		})
+				}
+			})
+		}
+	}
+})
