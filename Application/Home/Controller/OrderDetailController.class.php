2ce653ee42f8b043bb32e4f32d3630223bd303fa
sun-zw <619650434@qq.com>
2017-06-03 20:23:26 +0800

    update orderDetail

--- Application/Home/Controller/OrderDetailController.class.php
+++ Application/Home/Controller/OrderDetailController.class.php
@@ -11,5 +11,38 @@ class OrderDetailController extends Controller {
 	public function orderDetail(){
 		$this->display();
 	}
+	/*
+	*	进入页面获取信息；
+	*/
+	public function getInfo(){
+		$goods_id = cookie('goodsId');
+		$goods_id = 24;
+		$h_id = cookie('user');
+		$h_id = 1;
+		$goodsFlag = cookie('goodsFlag');//商品类别标志；
+		$order = M('order');
+		$images = M('images');
+		$res1 = array();
+		if($goodsFlag == 'n'){
+			$res1= $order ->field('f_order.*,n.n_name,n.h_id,h.h_nick,a.*')->join("LEFT JOIN f_address AS a ON a.d_id=f_order.d_id left join f_ngoods as n on n.n_id=f_order.n_id left join f_huser as h on h.h_id=n.h_id")->where("f_order.n_id={$goods_id} and f_order.h_id={$h_id}")->select();
+			$res2 = $images->where("n_id = $goods_id")->select();
+		}else if ($goodsFlag == 'p') {
+			$res1= $order ->field('f_order.*,p.p_name,p.h_id,h.h_nick,a.*')->join("LEFT JOIN f_address AS a ON a.d_id=f_order.d_id left join f_pgoods as p on p.p_id=f_order.p_id left join f_huser as h on h.h_id=p.h_id")->where("f_order.p_id={$goods_id} and f_order.h_id={$h_id}")->select();
+			$res2 = $images->where("p_id = $goods_id")->select();
+		}
+		
+		$this->ajaxreturn(array($res1,$res2,$goodsFlag));
+	}
+
+	public function saveOrderId(){
+		$orderId = I('orderId',0,'intval');
+		cookie('orderId',$orderId);
+
+		if(cookie('orderId') != 0){
+			echo 1;
+		}else{
+			echo 0;
+		}
+	}
 }
 ?>
